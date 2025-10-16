2c6e1b8946194daa7017f465facd0747372946b0
Ulf Slunga <98820233+UlfSlunga-Sinetiq@users.noreply.github.com>
2025-10-16 13:07:28 +0200

    feat: initial implementation of arbetsgivare masterdata

---
+++ src/test/java/fk/rimfrost/Arbetsgivare.java
@@ -0,0 +1,50 @@
+package fk.rimfrost;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+class ArbetsgivareTest
+{
+
+   @Test
+   void testArbetsgivareTrue()
+   {
+      String actualResponse = given()
+            .when().get("/arbetsgivare/1234")
+            .then()
+            .statusCode(200)
+            .extract()
+            .body()
+            .asString();
+
+      assertThat(actualResponse).isEqualToIgnoringWhitespace("""
+            {"result":true}
+            """);
+   }
+
+   @ParameterizedTest
+   @ValueSource(strings =
+   {
+         "19999", "19002"
+   })
+   void testArbetsgivareFalse(String personnummer)
+   {
+      String actualResponse = given()
+            .when().get("/arbetsgivare/" + personnummer)
+            .then()
+            .statusCode(200)
+            .extract()
+            .body()
+            .asString();
+
+      assertThat(actualResponse).isEqualToIgnoringWhitespace("""
+            {"result":false}
+            """);
+   }
+
+}
